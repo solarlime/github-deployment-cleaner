a1725e4877874569289261e319179609b6f38121
solarlime <solarlime@outlook.com>
2023-04-27 23:58:51 +0300

    Added a minimum layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FormEvent } from 'react';
+import { ReactComponent as Logo } from './assets/logo.svg';
+import styles from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function Form() {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(event.target);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="repo" placeholder="Fill in your repository name" required={true}></input>
+      <input type="text" name="user" placeholder="Fill in your GitHub username" required={true}></input>
+      <input type="text" name="token" placeholder="Fill in your GitHub token" required={true}></input>
+      <button type="submit">
+        Clean repository deployments
+      </button>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.logo}>
+        <a href="https://github.com/solarlime/github-deployment-cleaner/" target="_blank">
+          <Logo className={styles.logo__svg} />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>GitHub Deployment Cleaner</h1>
+      <p className={styles.description}>
+        This service helps to clean up repository deployments. It is possible to do it manually with the GitHub API. However, this one is simpler.
       </p>
-    </>
-  )
+      <Form/>
+      <div className={styles.help}>
+        <a href="https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token" target="_blank">
+          How you can get a GitHub token
+        </a>
+      </div>
+    </main>
+  );
 }
 
 export default App
