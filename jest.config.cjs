da9853900a92cb561342a51a71a3842d4a44535f
solarlime <solarlime@outlook.com>
2023-10-15 03:25:28 +0300

    Replaced jest with vitest: jest could not resolve *.svg?react (see vite-plugin-svgr)

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.svg$': '<rootDir>/__mocks__/svg.ts',
-    '\\.css$': 'identity-obj-proxy',
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteSvgr from 'vite-plugin-svgr';
@@ -5,5 +6,17 @@ import viteSvgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteSvgr(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.ts',
+      ],
+    },
+  },
   base: '/github-deployment-cleaner/',
 });
